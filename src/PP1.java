d57922c1d8ebb6f4e5fe0a2e4e605ff97515163b
myvin <myvinbarboza@gmail.com>
2020-05-01 15:59:05 +0530

    pp1 done

--- src/PP1.java
+++ src/PP1.java
@@ -34,42 +34,24 @@ import java.util.*;
                     }
 
                 }
-//                PriorityQueue<Integer> pqElements = new PriorityQueue<>((c,d)->d-c);
-//                PriorityQueue<Integer> pqIndex =  new PriorityQueue<>((c,d)->d-c);
-//                HashMap<Integer,Integer> hm = new HashMap<>();
-//                System.out.println(Arrays.toString(b));
-//                for (int i = 0; i <b.length ; i++) {
-//                    pqElements.add(a[i]);
-//                    pqIndex.add(b[i]);
-//                    hm.put(i,b[i]);
-//                }
-//                int c[] = new int[n];
-//                for (int i = 0; i <c.length ; i++) {
-//                    c[i]=
-//                }
-                TreeMap<Integer,Integer> map=new TreeMap<>();
-                TreeSet<Integer> set=new TreeSet<>();
+                //
                 System.out.println(Arrays.toString(b));
-                for (int i =0; i <b.length ; i++) {
-                    set.add(b[i]);
+                //System.out.println(a);
+             PriorityQueue<Integer> pq=new PriorityQueue<>((c,d)->(d-c));
+                PriorityQueue<Integer> we=new PriorityQueue<>((c,d)->(d-c));
+                for (int i = 0; i <a.length ; i++) {
+                    pq.add(a[i]);
+                    we.add(b[i]);
                 }
-                System.out.println(set);
-                set= (TreeSet<Integer>) set.descendingSet();
-                System.out.println(set);
-                Iterator<Integer> integerIterator = set.iterator();
+                System.out.println(pq+" "+we);
+                int sum=0;
+                while(!pq.isEmpty()){
+                    sum=sum+pq.poll()*we.poll();
+                }
+                System.out.println(sum);
 
-                int i=0;
-                while(integerIterator.hasNext()){
-                    map.put(integerIterator.next(),i++);
-
-                }
-                System.out.println(map);
-                Arrays.sort(a,Comparator.reverseOrder());
-                for (int j = 0; j <b.length ; j++) {
-                    b[j]=a[map.get(b[j])];
-                }
-                System.out.println(Arrays.toString(b));
-                System.out.println(aliceSum);
+               // System.out.println(Arrays.toString(b));
+                System.out.println(sum-aliceSum);
 
             }
         }catch (Exception e){
